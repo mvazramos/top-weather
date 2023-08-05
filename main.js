c363a8219f5b99a39f8a9db861cd5c87b300a113
mvazramos <vaz.ramosmiguel@gmail.com>
2023-08-05 13:54:08 +0100

    assets and functionality

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-const baseURL = "https://api.weatherapi.com/v1"
-const apiKey  = "2f201c79bcfe4992b90211353233107";
-const current = "/current.json"
+// WeatherAPI config
+const baseURL  = "https://api.weatherapi.com/v1"
+const apiKey   = "2f201c79bcfe4992b90211353233107";
+const current  = "/current.json"
 const forecast = "/forecast.json"
-const future  = "/future.json"
-const days =  document.querySelectorAll("#day");
-let location = "Lisbon"
-let forecastURL = `${baseURL}${forecast}?key=${apiKey}&q=${location}&days=3`
+const future   = "/future.json"
+
+// DOM 
+const days     = document.querySelectorAll("#day");
+const max_temp = document.querySelectorAll("#max_temp");
+const min_temp = document.querySelectorAll("#min_temp");
+const humidity = document.querySelectorAll("#humidity");
+
+// Config Display 
+const MAX_DAYS=3;
+
+
 
 async function getWeather(url){
     fetch(url,{mode: 'cors'})
@@ -13,17 +22,13 @@ async function getWeather(url){
             return response.json();
         })
         .then((response)=>{
+            let cityName= response.location.name
+            let countrName= response.location.country
+            displayLocation(`${cityName}, ${countrName}`);
             return response.forecast.forecastday;
-            
         })
         .then((response)=>{            
-            for (let i = 0; i < response.length; i++) {
-                days[i].innerHTML=response[i].date;
-                //console.log(response[i].day.maxtemp_c);
-                //console.log(response[i].day.mintemp_c);
-                //console.log(response[i].day.avghumidity)
-                
-            }
+            displayWeather(response);
         })
         .catch((err)=>{
             console.log(err);
@@ -31,7 +36,44 @@ async function getWeather(url){
     
 }
 
-getWeather(forecastURL);
 
+function displayWeather(weatherArray){
+    for (let i = 0; i < MAX_DAYS; i++) {
+        days[i].innerHTML=weatherArray[i].date;
+        max_temp[i].innerHTML=`${weatherArray[i].day.maxtemp_c}°C` ;
+        min_temp[i].innerHTML=`${weatherArray[i].day.mintemp_c}°C` ;
+        humidity[i].innerHTML=`${weatherArray[i].day.avghumidity}%` ;        
+    }
+}
+
+function displayLocation(location){
+    document.querySelector("#display-location").innerHTML=location;
+}
+
+function submitLocation(){
+    let location = document.querySelector("#search-location").value;
+    if(location!==""){
+        //set location url 
+        let forecastURL = `${baseURL}${forecast}?key=${apiKey}&q=${location}&days=3`
+        getWeather(forecastURL);
+        // clear input
+        document.querySelector("#search-location").value=""; 
+    }
+}
+
+function generateForecastURL(location, days){
+    let url = "";
+    return url
+}
+
+// addEventListener
+function addListeners(){
+    document.querySelector("#input-button").addEventListener("click", submitLocation);
+}
+
+// Set default location according to IP Address
+let defaultLocation =`${baseURL}${forecast}?key=${apiKey}&q=auto:ip&days=3`
+addListeners();
+getWeather(defaultLocation);
 
 
